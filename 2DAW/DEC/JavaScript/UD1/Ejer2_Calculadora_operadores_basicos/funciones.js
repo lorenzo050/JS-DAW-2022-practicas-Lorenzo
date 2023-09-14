09958e4a955eadcefaf5ac7fc90685a6701710d3
lorenzo050 <lorenutiel2002@gmail.com>
2023-09-14 10:34:10 +0200

    ejercicio calculadora

---
+++ 2DAW/DEC/JavaScript/UD1/Ejer2_Calculadora_operadores_basicos/funciones.js
@@ -0,0 +1,7 @@
+function calculadora(numero1,numero2) {
+    let resta = numero1 - numero2;
+    let multiplicacion = numero1 * numero2;
+    let division = numero1 / numero2;
+    alert("El resultado es: resta = " + resta + " , multiplicacion = "+ multiplicacion +" , division = "+ division);
+    return 0;
+    }
