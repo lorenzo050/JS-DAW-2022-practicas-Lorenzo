c731891a6483fccff3af22c61bb1da632d0a2f07
lorenzo050 <lorenutiel2002@gmail.com>
2023-09-19 08:51:10 +0200

    ejercicio 4 comparacion numeros corregido

--- 2DAW/DEC/JavaScript/UD1/Ejer4_Funciones_flecha_comparacion_numeros/comparacion_Numeros.js
+++ 2DAW/DEC/JavaScript/UD1/Ejer4_Funciones_flecha_comparacion_numeros/comparacion_Numeros.js
@@ -1,12 +1,12 @@
-comparacion = (num1,num2) => {
-    if (num1>num2){
-        numMayor =  num1;
-    } else {
-        numMayor = num2;
-    }
+comparacion = (num1, num2) => {
+  if (num1 > num2) {
+    numMayor = num1;
+  } else {
+    numMayor = num2;
+  }
 
-    return "El número mayor es : " + numMayor;
-}
+  return "El número mayor es : " + numMayor;
+};
 
-console.log(comparacion(5,3));
-console.log(comparacion(3,5));
+console.log(comparacion(5, 3));
+console.log(comparacion(3, 5));
