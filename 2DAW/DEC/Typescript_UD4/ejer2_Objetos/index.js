8dc2395d8d66753363921ead78d97cc5c4b19fa0
lorenzo050 <lorenutiel2002@gmail.com>
2023-12-05 09:29:20 +0100

    type objetos

---
+++ 2DAW/DEC/Typescript_UD4/ejer2_Objetos/index.js
@@ -0,0 +1,13 @@
+var DAW = {
+    profesor: "Toni",
+    alumno: "Lorenzo",
+    edad: "20",
+    pais: "España",
+    nacimiento: "2003",
+    mascota: false,
+    trabajo: {
+        empresa: "GFT",
+        sector: "Backend"
+    }
+};
+console.log(DAW);
