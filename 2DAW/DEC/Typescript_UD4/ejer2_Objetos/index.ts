b32682acd5f8ec38bb477864b392382d903eb0ce
lorenzo050 <lorenutiel2002@gmail.com>
2023-12-05 09:28:24 +0100

    type objetos

---
+++ 2DAW/DEC/Typescript_UD4/ejer2_Objetos/index.ts
@@ -0,0 +1,14 @@
+const DAW = {
+    profesor:"Toni",
+    alumno:"Lorenzo",
+    edad:"20",
+    pais:"España",
+    nacimiento:"2003",
+    mascota:false,
+    trabajo : {
+        empresa: "GFT",
+        sector: "Backend"
+    }
+}
+
+console.log(DAW);
