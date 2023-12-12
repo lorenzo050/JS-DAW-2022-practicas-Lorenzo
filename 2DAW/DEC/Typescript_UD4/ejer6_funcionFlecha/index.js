b92be4f0f749651a2917870414dc63fe2d71ab23
lorenzo050 <lorenutiel2002@gmail.com>
2023-12-12 08:53:37 +0100

    ejercicio 6

---
+++ 2DAW/DEC/Typescript_UD4/ejer6_funcionFlecha/index.js
@@ -0,0 +1,6 @@
+//cambiar a funcion flecha
+function suma(a, b) {
+    return (a + b);
+}
+//funcion flecha
+var suma2 = function (a, b) { return a + b; };
---
+++ 2DAW/DEC/Typescript_UD4/ejer6_funcionFlecha/index.ts
@@ -0,0 +1,8 @@
+//cambiar a funcion flecha
+function suma( a:number,b:number ){
+    return (a + b) 
+  }
+
+  
+  //funcion flecha
+  var suma2 = (a:number,b:number) => a + b ;
